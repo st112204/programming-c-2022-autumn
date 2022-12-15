fa0a4a557f76a523ade0a64b83076b9f0261fc29
Игорь Коровин <st112204@student.spbu.ru>
2022-12-15 12:33:50 +0300

    New test task

--- 2022.12.12-Test/Task1/Source.cpp
+++ 2022.12.12-Test/Task1/Source.cpp
@@ -1,11 +1,6 @@
 #include <iostream>
 
-// place maharaj in current cell
-// recursive place other maharajs in other cells, if it possible
-
-//spoiler: programm hasn't finished
-
-void mas_init(int** mas, int n, int i)
+void init(int** mas, int n, int i)
 {
 	if (i == n)
 	{
@@ -14,28 +9,22 @@ void mas_init(int** mas, int n, int i)
 	
 	mas[i] = new int[n]{ 0 };
 
-	mas_init(mas, n, i + 1);
+	init(mas, n, i + 1);
 }
 
-void mas_output(int** mas, int n, int i, int j)
+void init_init(int*** m_a_s, int n, int k, int i)
 {
-	if (i == n)
+	if (i == k)
 	{
 		return;
 	}
-	else if (j == n)
-	{
-		std::cout << "\n";
-		mas_output(mas, n, i + 1, 0);
-	}
-	else
-	{
-		std::cout << mas[i][j] << " ";
-		mas_output(mas, n, i, j + 1);
-	}
+
+	m_a_s[i] = new int*[n]{ nullptr };
+	init(m_a_s[i], n, 0);
+	init_init(m_a_s, n, k, i + 1);
 }
 
-void mas_delete(int** mas, int n, int i)
+void del(int** mas, int n, int i)
 {
 	if (i == n)
 	{
@@ -44,39 +33,176 @@ void mas_delete(int** mas, int n, int i)
 
 	delete[] mas[i];
 
-	mas_delete(mas, n, i + 1);
+	del(mas, n, i + 1);
 }
 
-//void cells_filled(int** mas, int i)
+void del_del(int*** m_a_s, int n, int k, int i)
+{
+	if (i == k)
+	{
+		return;
+	}
 
-void cells_attacked(int** mas, int n, int i, int j)
+	del(m_a_s[i], n, 0);
+
+	delete[] m_a_s[i];
+
+	del_del(m_a_s, n, k, i + 1);
+}
+
+void out(int** mas, int n, int i, int j)
+{
+	if (i == n)
+	{
+		std::cout << "\n";
+		return;
+	}
+	else if (j == n)
+	{
+		std::cout << "\n";
+		out(mas, n, i + 1, 0);
+	}
+	else
+	{
+		std::cout << mas[i][j] << " ";
+		out(mas, n, i, j + 1);
+	}
+}
+
+void copy(int** mas, int** m_a_s, int n, int i, int j)
 {
 	if (i == n)
 	{
 		return;
 	}
-
-
+	else if (j == n)
+	{
+		copy(mas, m_a_s, n, i + 1, 0);
+	}
+	else
+	{
+		m_a_s[i][j] = mas[i][j];
+		copy(mas, m_a_s, n, i, j + 1);
+	}
 }
 
-int maharaj(int** mas, int n, int k, int i);
+void clr(int** mas, int n, int i, int j)
+{
+	if (i == n)
+	{
+		return;
+	}
+	else if (j == n)
+	{
+		clr(mas, n, i + 1, 0);
+	}
+	else
+	{
+		mas[i][j] = 0;
+		clr(mas, n, i, j + 1);
+	}
+}
+
+void fill_attacked(int** mas, int n, int ii, int jj, int i, int j)
+{
+	if (i == n)
+	{
+		return;
+	}
+	else if (j == n)
+	{
+		fill_attacked(mas, n, ii, jj, i + 1, 0);
+	}
+	else
+	{
+		if
+		(
+			mas[i][j] == 0 &&
+			(
+				i == ii || j == jj || i + j == ii + jj || i - j == ii - jj ||
+				(i >= ii - 2 && i <= ii + 2 && j >= jj - 2 && j <= jj + 2)
+			)
+		)
+		{
+			mas[i][j] = 99;
+		}
+		fill_attacked(mas, n, ii, jj, i, j + 1);
+	}
+}
+
+void fill_next(int** mas, int n, int k0, int k, int i, int j)
+{
+	if (i == n)
+	{
+		return;
+	}
+	else if (j == n)
+	{
+		fill_next(mas, n, k0, k, i + 1, 0);
+	}
+	else
+	{
+		if ()
+	}
+}
+
+int count_free(int** mas, int n, int i, int j)
+{
+	if (i == n)
+	{
+		return 0;
+	}
+	else if (j == n)
+	{
+		return count_free(mas, n, i + 1, 0);
+	}
+	else if (mas[i][j] == 0)
+	{
+		return 1 + count_free(mas, n, i, j + 1);
+	}
+	else
+	{
+		return count_free(mas, n, i, j + 1);
+	}
+}
+
+int maharaj(int** mas, int*** m_a_s, int n, int k0, int k, int i, int j)
+{
+	//fill_attacked(mas, n, ii, jj, 0, 0);
+	if (k != k0)
+	{
+		copy(mas, m_a_s[k], n, 0, 0);
+	}
+	return 0;
+}
 
 int main(int argc, char* argv[])
 {
 	int n = 0;
 	int k = 0;
 	int** mas = nullptr;
+	int*** m_a_s = nullptr; // reserve copies of mas for maharaj algorithm
 
 	std::cin >> n;
 	std::cin >> k;
 
 	mas = new int*[n]{ nullptr };
-	mas_init(mas, n, 0);
+	init(mas, n, 0);
+	m_a_s = new int**[k]{ nullptr };
+	init_init(m_a_s, n, k, 0);
 
-	mas_output(mas, n, 0, 0);
+	std::cout << maharaj(mas, n, k + 1, k, 0, 0);
 
-	mas_delete(mas, n, 0);
+	/*fill_attacked(m_a_s[0], n, 3, 4, 0, 0);
+	fill_attacked(m_a_s[1], n, 5, 6, 0, 0);
+
+	out(m_a_s[0], n, 0, 0);
+	out(m_a_s[1], n, 0, 0);*/
+
+	del(mas, n, 0);
 	delete[] mas;
+	del_del(m_a_s, n, k, 0);
+	delete[] m_a_s;
 
 	return EXIT_SUCCESS;
 }
---
+++ 2022.12.12-Test/Task1_new/Source.cpp
@@ -0,0 +1,155 @@
+#include <iostream>
+#include <stdio.h>
+
+void init(int** mas, int n, int i)
+{
+	if (i == n)
+	{
+		return;
+	}
+
+	mas[i] = new int[n] { 0 };
+
+	init(mas, n, i + 1);
+}
+
+void border(int** mas, int n, int i, int j)
+{
+	if (i == n)
+	{
+		return;
+	}
+	else if (j == n)
+	{
+		border(mas, n, i + 1, 0);
+	}
+	else
+	{
+		if (i == 0 || j == 0 || i == n - 1 || j == n - 1)
+		{
+			mas[i][j] = -1;
+		}
+		border(mas, n, i, j + 1);
+	}
+}
+
+void out(int** mas, int n, int i, int j)
+{
+	if (i == n)
+	{
+		printf("\n");
+		return;
+	}
+	else if (j == n)
+	{
+		printf("\n");
+		out(mas, n, i + 1, 1);
+	}
+	else
+	{
+		printf("%3.d ", mas[i][j]);
+		out(mas, n, i, j + 1);
+	}
+}
+
+void del(int** mas, int n, int i)
+{
+	if (i == n)
+	{
+		return;
+	}
+
+	delete[] mas[i];
+
+	del(mas, n, i + 1);
+}
+
+// ( -> (0) right -> (1) down -> (2) left -> (3) up -> )
+int move(int m)
+{
+	if (m == 3)
+	{
+		return 0;
+	}
+	else
+	{
+		return ++m;
+	}
+}
+
+void spiral(int** mas, int n, int value, int m, int i, int j)
+{
+	if (value == n * n + 1)
+	{
+		return;
+	}
+
+	mas[i][j] = value;
+
+	if (m == 0) // right
+	{
+		if (mas[i][j + 1] != 0)
+		{
+			spiral(mas, n, value + 1, move(m), i + 1, j);
+		}
+		else
+		{
+			spiral(mas, n, value + 1, m, i, j + 1);
+		}
+	}
+	else if (m == 1) // down
+	{
+		if (mas[i + 1][j] != 0)
+		{
+			spiral(mas, n, value + 1, move(m), i, j - 1);
+		}
+		else
+		{
+			spiral(mas, n, value + 1, m, i + 1, j);
+		}
+	}
+	else if (m == 2) // left
+	{
+		if (mas[i][j - 1] != 0)
+		{
+			spiral(mas, n, value + 1, move(m), i - 1, j);
+		}
+		else
+		{
+			spiral(mas, n, value + 1, m, i, j - 1);
+		}
+	}
+	else if (m == 3) // up
+	{
+		if (mas[i - 1][j] != 0)
+		{
+			spiral(mas, n, value + 1, move(m), i, j + 1);
+		}
+		else
+		{
+			spiral(mas, n, value + 1, m, i - 1, j);
+		}
+	}
+}
+
+
+int main(int argc, char* argv[])
+{
+	int n = 0;
+	int** mas = nullptr;
+
+	std::cin >> n;
+
+	mas = new int* [n + 2] { nullptr };
+	init(mas, n + 2, 0);
+	border(mas, n + 2, 0, 0);
+
+	spiral(mas, n, 1, 0, 1, 1);
+
+	out(mas, n + 1, 1, 1);
+
+	del(mas, n + 2, 0);
+	delete[] mas;
+
+	return EXIT_SUCCESS;
+}
