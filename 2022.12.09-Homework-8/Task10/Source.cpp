202e7a01126740b11b5207e0073a611b3b34fbff
Игорь Коровин <st112204@student.spbu.ru>
2022-12-15 21:28:58 +0300

    Task 10 done

--- 2022.12.09-Homework-8/Task10/Source.cpp
+++ 2022.12.09-Homework-8/Task10/Source.cpp
@@ -1,28 +1,64 @@
-// in developing
-
 #include <iostream>
 
-void input_func(char** mas, int i, int n)
-{
-	if (n == 0)
-	{
-		return;
-	}
+void get(int(*mas)[2], int n, int i);
+int search(int(*mas)[2], int n, int k, int i, bool is_first);
 
-	std::cin >> mas[i][0] >> mas[i][1];
-
-	input_func(mas, i + 1, n - 1);
-}
 
 int main(int argc, char* argv[])
 {
-	int n = 0;
-	char mas[1000][2]{ 0 };
+    int n = 0;
+    int k = 0;
+    int mas[1000][2]{ 0 };
 
-	std::cin >> n;
+    std::cin >> n;
+    get(mas, n, 0);
+    std::cin >> k;
 
-	input_func(mas, 0, n);
+    std::cout << search(mas, n, k, 0, 1);
+
+    return 0;
+}
 
 
-	return EXIT_SUCCESS;
+void get(int(*mas)[2], int n, int i)
+{
+    if (i == n)
+    {
+        return;
+    }
+
+    std::cin >> mas[i][1] >> mas[i][0];
+
+    get(mas, n, i + 1);
+}
+
+int search(int(*mas)[2], int n, int k, int i, bool is_first)
+{
+    if (i == n)
+    {
+        return 0;
+    }
+
+    if (is_first)
+    {
+        if (mas[i][1] == k)
+        {
+            return 1 + search(mas, n, k, 0, 0);
+        }
+        else
+        {
+            return search(mas, n, k, i + 1, 1);
+        }
+    }
+    else
+    {
+        if (mas[i][0] == k)
+        {
+            return 1 + search(mas, n, mas[i][1], 0, 0);
+        }
+        else
+        {
+            return search(mas, n, k, i + 1, 0);
+        }
+    }
 }
